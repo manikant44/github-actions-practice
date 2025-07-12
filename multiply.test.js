5e4626f2222521d65bfc1677e1345bf85e19630f
manikant44 <manikantsharma44@gmail.com>
2025-07-12 19:33:38 +0100

    added eslint job and setup code

--- multiply.test.js
+++ multiply.test.js
@@ -1,5 +1,5 @@
-const multiply = require("./multiply");
+const multiply = require('./multiply');
 
-test("multiple 2 * 2 equal to 4 resulst", () => {
+test('multiple 2 * 2 equal to 4 resulst', () => {
   expect(multiply(2, 2)).toBe(4);
 });
--- sum.js
+++ sum.js
@@ -1,4 +1,7 @@
 function sum(a, b) {
   return a + b;
 }
+
+const mani = 'manikant name';
+
 module.exports = sum;
